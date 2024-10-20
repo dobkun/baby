24c9f9a757a5bd238e22b8c2115de63d6cc3fb7c
dobkun <dobkun@icloud.com>
2024-10-20 11:24:01 +0300

    second

---
+++ README.md
@@ -0,0 +1,1 @@
+um actually read the wiki page you 
---
+++ config.py
@@ -0,0 +1,42 @@
+EMBED_CONFIG = {
+    "baby": "",    # Maine Embed Title here 
+    "googoogaga": "",   # Main Embed Description here 
+    "color": 0xFF5733,   # Change embed color if you want (red)
+    "fields": [
+        {"name": "second", "value": "wrench dolls fragile snatch ring soft bells ten follow mighty", "inline": False},    # Embed Field → Juste Modify → Just edit the empty places
+        {"name": "test", "value": "test", "inline": False},
+        {"name": "test","value": "test", "inline": False},    # Exemple → "name": "Title 1", "value": "Hello, here is my message", "inline": False
+    ],
+    "image": "",   # Embed Icon url here → https://image.jpg
+    "footer": "",  # Embed Footer here 
+}
+
+SERVER_CONFIG = {
+    "new_name": "/caca/ - Cacaborea",  # New Server Name here 
+    "new_icon": "https://wiki.soyjak.st/images/d/de/SoobBaby.png",   # New Server Icon url here → https://image.jpg 
+    "new_description": "i ate the pink stuff on dada's floor, i luv pink stuff on dada floor",  # New Server Description here 
+}
+
+WEBHOOK_CONFIG = {
+    "default_name": "Baby",  # Webhook Name here 
+}
+
+
+AUTO_RAID_CONFIG = {
+    'num_channels': 15,  # Number of channels
+    'channel_type': 'text',  # text/voice
+    'channel_name': '',  # Channel name
+    'num_messages': 5,  # Number of message to spam
+    'message_content': 'second' # Spam Message
+}
+
+NO_BAN_KICK_ID = {
+    000000000000,       # Put Whitelist ID
+    111111111111,       # No Ban & No Kick
+    222222222222,
+}
+
+BOT_PRESENCE = {
+    "type": "watching",  # "playing", "listening", or "watching"
+    "text": "Tyrone"  # Your text presence
+}
